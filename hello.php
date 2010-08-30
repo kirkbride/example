7eaf92ce1c8f8541ba3d2623097ead586b92c688
kirkbride <admin@kirkbridebuildings.com>
2010-08-29 23:09:52 -0400

    Moved good-byes into conditional statement. Added comma.

--- hello.php
+++ hello.php
@@ -3,12 +3,13 @@
 $user_name = strip_tags($_REQUEST['un']);
 
 if (!($user_name == '')) {
-	echo "Hello world and especially you $username!";
+	echo "Hello world and especially you, $user_name!";
+	echo "Good-bye world and $user_name!";
 } else {
 	echo "Hello world!";
+	echo "Good-bye world!";
 }
 
-echo "Good-bye!";
 
 exit;
 
