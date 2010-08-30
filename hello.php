4c88c341aa74a6602f1f07eb8bd45be4885789b0
kirkbride <admin@kirkbridebuildings.com>
2010-08-29 23:10:22 -0400

    Fixed variable spelling.

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 $user_name = strip_tags($_REQUEST['un']);
 
 if (!($user_name == '')) {
-	echo "Hello world and especially you, $username!";
+	echo "Hello world and especially you, $user_name!";
 } else {
 	echo "Hello world!";
 }
