c47d4de6c50f5f71d477b973663523909d6ccbbd
kirkbride <admin@kirkbridebuildings.com>
2010-08-29 23:08:06 -0400

    Added comma in greeting.

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 $user_name = strip_tags($_REQUEST['un']);
 
 if (!($user_name == '')) {
-	echo "Hello world and especially you $username!";
+	echo "Hello world and especially you, $username!";
 } else {
 	echo "Hello world!";
 }
